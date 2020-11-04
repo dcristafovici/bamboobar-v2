e4898fa7532ce6f8403fe2ba62352697cfaa4bbf
denibudeyko <denbudeyko@gmail.com>
2020-11-04 06:46:43 +0200

    add frontend

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express')
-const path = require('path')
+const mongoose = require('mongoose')
 const config = require('config')
+const cors = require('cors')
 
 const app = express()
-const PORT = config.gt
+const PORT = config.get('port') || 5000
+app.use(express.json({extended: true}))
+app.use(cors())
+
+async function start() {
+  try{
+    await mongoose.connect(
+      config.get('mongoURI'),
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    )
+    app.listen(PORT, ()=> console.log(`Start with port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+
+start()
